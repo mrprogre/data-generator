e059fdad8289114044c515aca51b6048ab10d6dc
Dmitry <rps_project@mail.ru>
2022-08-26 15:04:05 +0300

    info: рефакторинг + нормальные русские фамилии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,26 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static final FileWriter WRITER = new FileWriter();
+    private static final Randomizer RANDOM = new Randomizer();
+    private static final int ROWS_GENERATE_NUM = 1000;
+    private static final int MAX_AGE = 80;
+    private static final int MIN_GRADE = 2;
+    private static final int MAX_GRADE = 5;
 
     public static void main(String[] args) throws IOException {
 //        int firstParam = Integer.parseInt(args[0]);
-        int firstParam = 10000;
-        FileWriter fileWriter = new FileWriter();
-        Randomizer random = new Randomizer();
+
         PeopleNames names = new PeopleNames();
         List<String> rows = new ArrayList<>();
 
-        String row;
-        for (int i = 0; i < firstParam; i++) {
 
-            row = random.getRandomInt(80) + ";" +
-                    random.getRandomDouble(2, 5) + ";" +
+        String row;
+        for (int i = 0; i < ROWS_GENERATE_NUM; i++) {
+
+            row = RANDOM.getRandomInt(MAX_AGE) + ";" +
+                    RANDOM.getRandomDouble(MIN_GRADE, MAX_GRADE) + ";" +
                     names.getRandomManName() + " " + names.getManSurname() + ";" +
                     names.getRandomWomanName() + " " + names.getWomanSurname() + ";" +
                     "\n";
 
             rows.add(row.replace(".", ","));
         }
-        fileWriter.save(rows);
+        WRITER.save(rows);
     }
 }
